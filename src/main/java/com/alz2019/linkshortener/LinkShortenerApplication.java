ff0b3b832d6e7140e09f68587f334516b1462182
alz2019 <az2019git@rambler.ru>
2021-02-02 06:45:27 +0300

    Initial commit

---
+++ src/main/java/com/alz2019/linkshortener/LinkShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.alz2019.linkshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkShortenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alz2019/linkshortener/LinkShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alz2019.linkshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
