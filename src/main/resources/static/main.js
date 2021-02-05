5b20e5358d09b4c8b0b8f8c60bf7c965cd5bd42d
alz2019 <az2019git@rambler.ru>
2021-02-05 04:45:08 +0300

    Add index.html

---
+++ src/main/resources/static/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $.ajax({
+            url: 'http://localhost:8080/short?long_link='.concat($("#urlinput").val()),
+            type: 'get',
+            dataType: 'JSON',
+            success: function (response) {
+                var hash = response.short_link;
+                $("#shorturl").val("http://localhost:8080/?short_link=".concat(hash));
+            }
+        });
+    });
+});
