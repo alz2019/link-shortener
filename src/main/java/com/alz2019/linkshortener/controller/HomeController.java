2deed5bf240d643e293e18d3964e8a4a10366ad3
alz2019 <az2019git@rambler.ru>
2021-02-13 13:31:32 +0300

    Clarify GetMapping

--- src/main/java/com/alz2019/linkshortener/controller/HomeController.java
+++ src/main/java/com/alz2019/linkshortener/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping(value = "/home")
+    @GetMapping("/home")
     public String loadIndex() {
         return "index";
     }
