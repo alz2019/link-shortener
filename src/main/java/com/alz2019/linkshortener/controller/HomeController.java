b68fa8aada87ae701fe8c2fcaa630da80c41728d
alz2019 <az2019git@rambler.ru>
2021-02-13 13:22:56 +0300

    Configure GetMapping

--- src/main/java/com/alz2019/linkshortener/controller/HomeController.java
+++ src/main/java/com/alz2019/linkshortener/controller/HomeController.java
@@ -1,12 +1,11 @@
 package com.alz2019.linkshortener.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @GetMapping(value = "/home")
     public String loadIndex() {
         return "index";
     }
