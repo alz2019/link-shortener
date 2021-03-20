d901f5c3ae74dc8808cd5bf25c864a685695d9ac
alz2019 <az2019git@rambler.ru>
2021-03-20 04:54:22 +0300

    Remove Repository annotation

--- src/main/java/com/alz2019/linkshortener/repository/LinkRepository.java
+++ src/main/java/com/alz2019/linkshortener/repository/LinkRepository.java
@@ -4,7 +4,6 @@ import com.alz2019.linkshortener.model.Link;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface LinkRepository extends CrudRepository<Link, String> {
     Link save(Link link);
 
