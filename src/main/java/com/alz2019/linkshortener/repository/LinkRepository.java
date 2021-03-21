2d1282c5bbb2006f2bc9626c746c73326d92805b
alz2019 <az2019git@rambler.ru>
2021-03-21 08:38:35 +0300

    Remove unnecessary import

--- src/main/java/com/alz2019/linkshortener/repository/LinkRepository.java
+++ src/main/java/com/alz2019/linkshortener/repository/LinkRepository.java
@@ -2,7 +2,6 @@ package com.alz2019.linkshortener.repository;
 
 import com.alz2019.linkshortener.model.Link;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 public interface LinkRepository extends CrudRepository<Link, String> {
     Link save(Link link);
